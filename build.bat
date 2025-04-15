095c35563a6f5ee0d8063f506fd50905f98abdb9
N0tZetsu <emamainn@gmail.com>
2025-04-15 14:27:11 -0300

    Add build.bat 1744738029559

--- build.bat
+++ build.bat
@@ -1,10 +1,19 @@
 @echo off
-REM Build script for compiling csgo_cheat.cpp into a DLL using MinGW
+REM Build script for compiling CS:GO cheat DLL using MinGW
 
 REM Set path to your MinGW bin directory if not in PATH
 REM set PATH=C:\MinGW\bin;%PATH%
 
-g++ -shared -o csgo_cheat.dll csgo_cheat.cpp -ld3d9 -ld3dx9 -limm32 -lgdi32 -luser32 -lkernel32 -std=c++17 -DUNICODE -D_UNICODE -I./imgui -I./imgui/backends
+echo Building CS:GO cheat DLL...
+
+REM Compile all source files
+g++ -c main.cpp aimbot.cpp esp.cpp menu.cpp -I. -std=c++17
+
+REM Link object files into DLL
+g++ -shared -o csgo_cheat.dll main.o aimbot.o esp.o menu.o -ld3d9 -limm32 -lgdi32 -luser32 -lkernel32 -std=c++17 -DUNICODE -D_UNICODE
+
+REM Clean up object files
+del *.o
 
 echo Build complete: csgo_cheat.dll
 pause
