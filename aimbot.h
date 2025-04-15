cc6984232631aae7805acf00883587a0986d6db3
N0tZetsu <emamainn@gmail.com>
2025-04-15 14:27:10 -0300

    Add aimbot.h 1744738028531

---
+++ aimbot.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void InitializeAimbot();
+void RunAimbot();
+void SetAimbotEnabled(bool enabled);
+void SetAimbotFOV(float fov);
+void SetSmoothFactor(float smooth);
+bool IsAimbotEnabled();
