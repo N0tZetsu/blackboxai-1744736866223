12ff1742d57146079813ffb807b4866a439821d0
N0tZetsu <emamainn@gmail.com>
2025-04-15 16:15:14 -0300

    Add build.bat 1744744512366

--- build.bat
+++ build.bat
@@ -4,29 +4,39 @@ REM Build script for compiling CS2 cheat DLL using MinGW
 REM Set path to your MinGW bin directory if not in PATH
 REM set PATH=C:\MinGW\bin;%PATH%
 
-REM Set Vulkan SDK path
-set VULKAN_SDK=C:\Users\Admin\VulkanSDK
-
 echo Building CS2 cheat DLL...
 
+REM Create build directory if it doesn't exist
+if not exist "build" mkdir build
+
 REM Compile all source files with debug info
-g++ -c main.cpp aimbot.cpp esp.cpp menu.cpp memory.cpp csgo_cheat.cpp -I. -I"%VULKAN_SDK%\Include" -std=c++17 -g
+g++ -c main.cpp ^
+    aimbot.cpp ^
+    esp.cpp ^
+    menu.cpp ^
+    memory.cpp ^
+    csgo_cheat.cpp ^
+    -I. ^
+    -I"C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include" ^
+    -std=c++17 -g
 
 REM Link object files into DLL with all required libraries
-g++ -shared -o cs2_cheat.dll main.o aimbot.o esp.o menu.o memory.o csgo_cheat.o ^
-    -L"%VULKAN_SDK%\Lib" -lvulkan-1 -lgdi32 -luser32 -lkernel32 ^
+g++ -shared -o build/cs2_cheat.dll *.o ^
+    -L"C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86" ^
+    -ld3d9 -ld3dx9 -lgdi32 -luser32 -lkernel32 ^
     -std=c++17 -DUNICODE -D_UNICODE
 
 REM Clean up object files
 del *.o
 
-echo Build complete: cs2_cheat.dll
+echo Build complete: build/cs2_cheat.dll
 
 REM Instructions
 echo.
-echo El cheat ha sido compilado. Para usarlo:
-echo 1. Inyectar cs2_cheat.dll en CS2
-echo 2. Usar la tecla Insert para mostrar/ocultar el menu
-echo 3. Configurar Aimbot y ESP desde el menu
+echo El cheat ha sido compilado en la carpeta 'build'.
+echo Para usarlo:
+echo 1. El archivo DLL esta en: build/cs2_cheat.dll
+echo 2. Inyectar el DLL en CS2
+echo 3. Usar la tecla Insert para mostrar/ocultar el menu
 echo.
 pause
