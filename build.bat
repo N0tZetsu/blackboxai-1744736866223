4cee990f2b6701c811fc570b56e22eb5691b2cab
N0tZetsu <emamainn@gmail.com>
2025-04-15 16:19:07 -0300

    Add build.bat 1744744745681

--- build.bat
+++ build.bat
@@ -1,42 +1,56 @@
 @echo off
-REM Build script for compiling CS2 cheat DLL using MinGW
+REM Build script for compiling CS2 cheat DLL using Visual Studio
 
-REM Set path to your MinGW bin directory if not in PATH
-REM set PATH=C:\MinGW\bin;%PATH%
+REM Setup VS2019 environment (ajusta la ruta según tu versión de Visual Studio)
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 
 echo Building CS2 cheat DLL...
 
 REM Create build directory if it doesn't exist
 if not exist "build" mkdir build
 
-REM Compile all source files with debug info
-g++ -c main.cpp ^
+REM Compile all source files
+cl /c /MD /I"C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include" ^
+    main.cpp ^
     aimbot.cpp ^
     esp.cpp ^
     menu.cpp ^
     memory.cpp ^
     csgo_cheat.cpp ^
-    -I. ^
-    -I"C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Include" ^
-    -std=c++17 -g
+    hooks.cpp ^
+    options.cpp
 
-REM Link object files into DLL with all required libraries
-g++ -shared -o build/cs2_cheat.dll *.o ^
-    -L"C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86" ^
-    -ld3d9 -ld3dx9 -lgdi32 -luser32 -lkernel32 ^
-    -std=c++17 -DUNICODE -D_UNICODE
+REM Link to create DLL
+link /DLL /OUT:build\cs2_cheat.dll ^
+    /LIBPATH:"C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86" ^
+    main.obj ^
+    aimbot.obj ^
+    esp.obj ^
+    menu.obj ^
+    memory.obj ^
+    csgo_cheat.obj ^
+    hooks.obj ^
+    options.obj ^
+    d3d9.lib ^
+    d3dx9.lib ^
+    user32.lib ^
+    kernel32.lib ^
+    gdi32.lib
 
 REM Clean up object files
-del *.o
+del *.obj
 
-echo Build complete: build/cs2_cheat.dll
-
-REM Instructions
 echo.
-echo El cheat ha sido compilado en la carpeta 'build'.
-echo Para usarlo:
-echo 1. El archivo DLL esta en: build/cs2_cheat.dll
-echo 2. Inyectar el DLL en CS2
-echo 3. Usar la tecla Insert para mostrar/ocultar el menu
+echo Para compilar necesitas:
+echo 1. Visual Studio 2019 (Community Edition es gratis)
+echo    - Descargar de: https://visualstudio.microsoft.com/vs/older-downloads/
+echo    - Durante la instalación, seleccionar "Desarrollo de escritorio con C++"
+echo.
+echo 2. DirectX SDK (June 2010)
+echo    - Descargar de: https://www.microsoft.com/en-us/download/details.aspx?id=6812
+echo.
+echo 3. Después de instalar:
+echo    - Ejecutar este archivo build.bat
+echo    - El DLL se creará en la carpeta build
 echo.
 pause
